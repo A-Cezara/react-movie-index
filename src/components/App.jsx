329794a3433577a29e00d158024cf79c6eba372c
A-Cezara <alexandracezara@yahoo.ro>
2024-09-16 16:01:27 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,12 @@ export default function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/react-movie-index" element={<HomePage />} />
 
-        <Route path="/movie/:movieId" element={<MovieDetail />}>
+        <Route
+          path="/react-movie-index/movie/:movieId"
+          element={<MovieDetail />}
+        >
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
